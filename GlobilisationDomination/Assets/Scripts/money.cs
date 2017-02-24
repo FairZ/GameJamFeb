7f21380034e8ae746b413816a6f7d9cb33cb5cc6
pringle285 <rhys.male@blueyonder.co.uk>
2017-02-24 20:05:36 +0000

    Removed library folder

---
+++ GlobilisationDomination/Assets/Scripts/money.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class money : MonoBehaviour {
+
+	public static int moneyValue; 
+
+	Text moneyText;
+
+	void Update (){
+		moneyText = GetComponent<Text> (); 
+
+		moneyText = moneyValue.ToString();
+
+	}
+}
