a42de0dade813866ec3f7ac502d240df1ecdaf1b
FairZ <fairsy2104@gmail.com>
2017-02-25 11:51:21 +0000

    Added sun rotation

---
+++ GlobilisationDomination/Assets/Scripts/SunRotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunRotate : MonoBehaviour {
+
+	public GameObject World;
+
+	void Update () {
+		transform.RotateAround (World.transform.position, Vector3.up, 10 * Time.deltaTime);
+	}
+}
