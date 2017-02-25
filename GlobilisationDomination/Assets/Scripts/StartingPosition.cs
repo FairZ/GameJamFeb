748784233719f3f808237c8d6641466ee19a924c
pringle285 <rhys.male@blueyonder.co.uk>
2017-02-25 13:44:23 +0000

    no idea

--- GlobilisationDomination/Assets/Scripts/StartingPosition.cs
+++ GlobilisationDomination/Assets/Scripts/StartingPosition.cs
@@ -3,13 +3,8 @@ using System.Collections;
 
 public class StartingPosition : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+
+
 }
