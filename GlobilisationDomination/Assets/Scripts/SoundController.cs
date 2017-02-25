d40788b5ca1b66104302266efd740ddffd175102
Grigler <graham.rigler@gmail.com>
2017-02-25 21:14:57 +0000

    Test

--- GlobilisationDomination/Assets/Scripts/SoundController.cs
+++ GlobilisationDomination/Assets/Scripts/SoundController.cs
@@ -4,8 +4,5 @@ using System.Collections;
 
 public class SoundController : MonoBehaviour 
 {
-	public static AudioClip factoryPlace;
-	public static AudioClip moneyBagAppear;
-	public static AudioClip moneyBagCollect;
-	public static AudioClip bgMusic;
+
 }
