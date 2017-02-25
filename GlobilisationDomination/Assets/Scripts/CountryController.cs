362499d63607e6ccb75608b2362109e3b3977732
Grigler <graham.rigler@gmail.com>
2017-02-25 15:06:45 +0000

    Factory Adding

--- GlobilisationDomination/Assets/Scripts/CountryController.cs
+++ GlobilisationDomination/Assets/Scripts/CountryController.cs
@@ -20,6 +20,8 @@ public class CountryController : MonoBehaviour {
 	public GameObject factoryPrefab;
 	private List<GameObject> factoryList;
 
+	public bool isAddingFactory = false;
+
 	void Start()
 	{
 		//will need changing to balance
@@ -28,6 +30,27 @@ public class CountryController : MonoBehaviour {
 		Button FactoryLimitUpgrade = GameObject.Find ("upgradeButton").GetComponent<Button>();
 		FactoryLimitUpgrade.onClick.AddListener (UpgradeFactoryLimitInCountry);
 		CountryFactoryLimitText.text = ("Factory Limit: " + FactoryLimit.ToString ());
+	}
+
+	void FixedUpdate()
+	{
+		if(isAddingFactory)
+		{
+			if(Input.GetMouseButton(0))
+			{
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+				if(Physics.Raycast(ray, out hit))
+				{
+					if (hit.collider.gameObject == selectedCountry)
+						AddFactory (hit.point);
+				}
+
+			}
+
+		}
+
 	}
 
 	void UpgradeFactoryLimitInCountry()
@@ -40,9 +63,31 @@ public class CountryController : MonoBehaviour {
 		}
 	}
 
-	void AddFactory()
+	public void SwitchAddingState()
 	{
-		
+		isAddingFactory = !isAddingFactory;
+	}
+
+	void AddFactory(Vector3 _loc)
+	{
+		GameObject pRef = GameObject.Find ("globe");
+
+		Vector3 dir = (pRef.transform.position + _loc).normalized;
+
+		GameObject fRef = Instantiate (factoryPrefab, _loc + (dir * 10.0f), new Quaternion ()) as GameObject;
+
+		RaycastHit hit;
+		if(fRef.GetComponent<Rigidbody>().SweepTest(-dir, out hit))
+		{
+			if(hit.collider.gameObject == selectedCountry)
+			{
+				fRef.transform.position = hit.point;
+				fRef.transform.up = dir;
+				SwitchAddingState ();
+			}
+		}
+
+
 	}
 
 
