6012de9c5843fa42f40978b6c43a41396f8cd691
Grigler <graham.rigler@gmail.com>
2017-02-25 16:58:17 +0000

    Moneybag Movement

--- GlobilisationDomination/Assets/Scripts/collectMoney.cs
+++ GlobilisationDomination/Assets/Scripts/collectMoney.cs
@@ -5,6 +5,58 @@ public class collectMoney : MonoBehaviour {
 
 	public int value; 
 
+	private bool isRising = true;
+
+	private Material[] matRefs;
+
+	//Relative to the factory
+	private const float heightLimit = 10f;
+	private float startPos;
+
+	private const float timeoutMax = 10.0f;
+	private float timeout = timeoutMax;
+
+	void Start()
+	{
+		startPos = this.transform.position;
+		matRefs = this.GetComponents<Material> ();
+	}
+
+	void Update()
+	{
+		if (isRising)
+		{
+			if (Vector3.Distance (this.transform.position, startPos) > heightLimit) 
+			{
+				isRising = false;
+			}
+			else 
+			{
+				//Move it by delta time
+			}
+		}
+		else
+		{
+			timeout -= Time.deltaTime;
+
+			if(timeout <= 0.0f)
+			{
+				Destroy (this.gameObject);
+			}
+			else
+			{
+				float alpha = (timeout / timeoutMax) * 100.0f;
+				foreach (Material m in matRefs) 
+				{
+					m.color.a = alpha;
+				}
+			}
+		}
+
+		this.transform.Translate (this.transform.up * Time.deltaTime);
+
+	}
+
 	public void Collect()
 	{
 		//Do some stuff here with money and object deletion
