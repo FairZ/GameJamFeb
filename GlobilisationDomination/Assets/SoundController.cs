e778ea6f3b8da185d415f364fe662cb639c0a4bf
Grigler <graham.rigler@gmail.com>
2017-02-25 16:02:24 +0000

    UI

---
+++ GlobilisationDomination/Assets/SoundController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	public AudioClip factoryPlace;
+	public AudioClip moneyBagAppear;
+	public AudioClip moneyBagCollect;
+	public AudioClip bgMusic;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
